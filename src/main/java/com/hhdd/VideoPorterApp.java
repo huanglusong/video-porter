c16b4d9b78c6d147bca1baeb70b015be94fb71f7
huanglusong <sj15659794782@gmail.com>
2023-01-15 23:05:27 +0800

    init commit

---
+++ src/main/java/com/hhdd/VideoPorterApp.java
@@ -0,0 +1,13 @@
+package com.hhdd;
+
+/**
+ * @Author huanghedidi
+ * @Date 2023/1/15 23:05
+ */
+public class VideoPorterApp {
+
+    public static void main(String[] args) {
+        System.out.println("hello world。。。。。。。。。");
+    }
+
+}
